5bbbb738b30aa9fa03cec61f4fb65a167249bb79
Udayon-Paul-Dhrubo <1805109@ugrad.cse.buet.ac.bd>
2022-02-08 23:34:40 +0600

    name modified

---
+++ controllers/teacher.js
@@ -0,0 +1,5 @@
+const UserRepository = require('../repository/user-info').UserRepository;
+const userRepository = new UserRepository();
+
+const Category_Course_Teacher_Info_Repository = require('../repository/category-course-info').Category_Course_Teacher_Info_Repository;
+const infoRepository = new Category_Course_Teacher_Info_Repository();
---
+++ routes/student.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const studentController = require('../controllers/student')
+
+
+//const router = express.Router()
+const router = require('express-promise-router')()
+
+router.get('/user/:ID/', )
+
+router.get('/user/:ID/about', )
+
+
+
+
+
+module.exports = router
